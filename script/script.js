99da1b845147adce77405231d94c74f1d8a221bb
kinkidycodes <51934846+kinkidycodes@users.noreply.github.com>
2020-07-13 13:10:47 +0100

    j. file added

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+// const navList = document.getElementsByClassName("nav-list");
+// navList.addEventListener("click", myFunction);
+
+// function myFunction() {
+//   alert ("Hello World!");
+// }
+
+const active = document.getElementById('active');
+active.addEventListener('click', function () {
+    active.style.backgroundColor= '#03035e';
+});
