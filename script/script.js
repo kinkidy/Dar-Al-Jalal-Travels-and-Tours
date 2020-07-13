54a14f4040470b73a9ed32d2b6adb7d8d9f01af6
kinkidycodes <51934846+kinkidycodes@users.noreply.github.com>
2020-07-13 13:00:58 +0100

    chore:nav background

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// let navList = document.getElementsByClassName('.nav-list');
+
+// navList.addEventListener('click', function () {
+//     alert('Hello Africa');
+// });
+// const navList = document.getElementsByClassName("nav-list");
+// navList.addEventListener("click", myFunction);
+
+// function myFunction() {
+//   alert ("Hello World!");
+// }
+
+const active = document.getElementById('active');
+active.addEventListener('click', function () {
+    active.style.backgroundColor= '#03035e';
+});
