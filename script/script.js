c5424b5cb26e1892559bc19914e7a787556f4ebe
kinkidycodes <51934846+kinkidycodes@users.noreply.github.com>
2020-07-14 09:56:50 +0100

    Hotel and visa page restructured

--- script/script.js
+++ script/script.js
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-// let navList = document.getElementsByClassName('.nav-list');
+let navList = document.getElementsByClassName('nav-list');
 
-// navList.addEventListener('click', function () {
-//     alert('Hello Africa');
-// });
-=======
+navList[2].addEventListener('click', function (){
+    console.log("Energy God");
 
->>>>>>> 0f5b7b2621c3ba1e9502968c113da3876f2fc313
-// const navList = document.getElementsByClassName("nav-list");
-// navList.addEventListener("click", myFunction);
-
-// function myFunction() {
-//   alert ("Hello World!");
-// }
-
-const active = document.getElementById('active');
-active.addEventListener('click', function () {
-    active.style.backgroundColor= '#03035e';
-});
+})
+// console.log("Energy God");
